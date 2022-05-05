17782073f02b2e53428407b3b8033df3747c904a
Nikolay V <nv-mail@mail.ru>
2022-05-06 00:48:16 +0300

    is done. version 2 for module 5 exercise 2.

--- hw_M5_E2v2.py
+++ hw_M5_E2v2.py
@@ -1,1 +1,7 @@
-print(' start working ! ')
+print(' start working ! welcome to bia : Module 5 Exercise 2 version 2 ! ')
+
+l = [1, 4, 1, 6, 'hello', 'a', 5, 'hello']
+
+l_unique = []
+l_unique = list(set(l))
+print('l_unique by set : ', l_unique)
